79cb5e589790b94ec91937a89caef98310341570
JerryNyoike <jngugi31@gmail.com>
2020-01-14 07:45:46 +0300

    Define tokens for the language

---
+++ tokens.c
@@ -0,0 +1,29 @@
+typedef struct Token {
+	char *Type;
+	char *Literal;	
+} Token;
+
+enum {
+	EOF = -1,
+	ILLEGAL,
+	
+	// Identifiers + literals
+	IDENT,
+	INT,
+
+	// Operators
+	ASSIGN,
+	PLUS,
+
+	// Delimiters
+	COMMA,
+	SEMICOLON,
+
+	LPAREN,
+	RPAREN,
+	LBRACE,
+	RBRACE,
+};
+
+#define FUNCTION "FUNCTION"
+#define LET "LET"
