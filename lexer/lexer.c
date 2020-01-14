f775d5ae27d70a391f992ed55dc784e8081f56e1
JerryNyoike <jngugi31@gmail.com>
2020-01-14 09:36:57 +0300

    Add function to get the next token from the input

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -1,5 +1,5 @@
-#include "token/tokens.c"
 #include <string.h>
+#include "../token/tokens.c"
 
 typedef struct Lexer {
 	char *input;
@@ -30,3 +30,50 @@ Lexer newLexer(char *input)
 	return lx;
 }
 
+/* newToken : */
+Token newToken(int tokenType, char *ch)
+{
+	Token tk;
+	tk.Type = tokenType;
+	tk.Literal = ch;
+	return tk;
+}
+
+/* nextToken : return token depending on char under examination */
+Token nextToken(Lexer lx)
+{
+	Token tk;
+
+	switch (lx.ch) {
+		case '=':
+			tk = newToken(ASSIGN, &lx.ch);
+			break;
+		case ';':
+			tk = newToken(SEMICOLON, &lx.ch);
+			break;
+		case '(':
+			tk = newToken(LPAREN, &lx.ch);
+			break;
+		case ')':
+			tk = newToken(RPAREN, &lx.ch);
+			break;
+		case ',':
+			tk = newToken(COMMA, &lx.ch);
+			break;
+		case '+':
+			tk = newToken(PLUS, &lx.ch);
+			break;
+		case '{':
+			tk = newToken(LBRACE, &lx.ch);
+			break;
+		case '}':
+			tk = newToken(LBRACE, &lx.ch);
+			break;
+		case 0:
+			tk.Literal = "";
+			tk.Type = EOF;
+	}
+
+	readChar(lx);
+	return tk;
+}
