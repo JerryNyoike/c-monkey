b67fafe0f34d0bc3003e8881ce39d17926c99202
JerryNyoike <jngugi31@gmail.com>
2020-01-17 14:22:01 +0300

    Add init function to make sure the literal pointer is initialised before use

--- token/tokens.c
+++ token/tokens.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #define FUNCTION "FUNCTION"
 #define LET "LET"
 
@@ -5,6 +6,14 @@ typedef struct Token {
 	int Type;
 	char *Literal;	
 } Token;
+
+/* initToken: set the Literal to NULL to ensure it's always initialised */
+Token initToken()
+{
+	Token tk;
+	tk.Literal = NULL;
+	return tk;
+}
 
 enum {
 	EOF,
